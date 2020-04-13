d7e7b2937a1db1dbc4c55d949b3146d294a79534
Janko <mailodjanka@gmail.com>
2020-04-13 18:17:21 +0200

    Generates Task Serializer, defines prodcts and total method Co-authored-by: Carltesio <carlos.delgado@hotmail.it> Co-authored-by: MadFarmer101 <mailodjanka@gmail.com>

---
+++ app/serializers/task_serializer.rb
@@ -0,0 +1,14 @@
+class TaskSerializer < ActiveModel::Serializer
+  attributes :id, :products, :total, :order_total
+
+  def products
+    object.task_items.group_by(&:product_id).map do |_key, value|
+      product = value.uniq(&:product_id)[0].product
+      { amount: value.size, name: product.name, total: (value.size * product.price) }
+    end
+	end
+
+  def total
+    object.task_items.joins(:product).sum('products.price')
+  end
+end
