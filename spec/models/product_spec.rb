210ebd519ce8ad485f936e5ddcfb6b95bb10a5b0
carlos <carlos.delgado@hotmail.it>
2020-04-13 11:00:31 +0200

    adding validation of name and price Co-authored-by: Carltesio <carlos.delgado@hotmail.it> Co-authored-by: MadFarmer101 <mailodjanka@gmail.com>

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe Product, type: :model do
   it "should have valid factory" do
     expect(create(:product)).to be_valid
   end
+
+  describe "Database table" do
+    it {is_expected.to have_db_column :name}
+    it {is_expected.to have_db_column :price}
+  end
+
+  describe "Validations" do
+    it {is_expected.to validate_presence_of :name}
+    it {is_expected.to validate_presence_of :price}
+  end
 end
