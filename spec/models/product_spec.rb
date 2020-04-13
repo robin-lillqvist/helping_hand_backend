b081a8044ffc2cf83b83b50b81b057669747300f
carlos <carlos.delgado@hotmail.it>
2020-04-13 11:12:08 +0200

    adding the model spec, testing creatinón of product Co-authored-by: Carltesio <carlos.delgado@hotmail.it> Co-authored-by: MadFarmer101 <mailodjanka@gmail.com>

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -14,4 +14,17 @@ RSpec.describe Product, type: :model do
     it {is_expected.to validate_presence_of :name}
     it {is_expected.to validate_presence_of :price}
   end
+
+  describe "check if new product is being created" do
+    before do
+      @product1 = Product.new(name: "bread", price: 10)
+    end
+    it "expects to contain the product name" do
+      expect(@product1.name).to eq("bread")
+    end
+    it "expects to contain the product price" do
+      expect(@product1.price).to eq(10)
+    end
+
+  end
 end
