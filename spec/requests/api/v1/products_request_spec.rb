8a8b939dfd0ecf2764c016bcc7cee78bd524d8e6
carlos <carlos.delgado@hotmail.it>
2020-04-13 11:42:37 +0200

    adding the test to see the product created Co-authored-by: Carltesio <carlos.delgado@hotmail.it> Co-authored-by: MadFarmer101 <mailodjanka@gmail.com>

--- spec/requests/api/v1/products_request_spec.rb
+++ spec/requests/api/v1/products_request_spec.rb
@@ -11,5 +11,9 @@ RSpec.describe "Api::V1::ProductsController", type: :request do
     it 'returns a 200 response status' do
       expect(response).to have_http_status 200
     end
+
+    it 'returns 5 products' do
+      expect(response_json["products"].count).to eq 5
+    end
   end
 end
