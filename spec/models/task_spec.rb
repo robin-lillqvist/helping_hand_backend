87a9bfebe58339fe317b7c1d66f2d50758ee4ba3
pierre-1 <pierreantoine.tiberi@gmail.com>
2020-04-14 11:51:51 +0200

    Adds user_can_confirm_task spec Co-authored-by: Carltesio <carlos.delgado@hotmail.it> Co-authored-by: MadFarmer101 <mailodjanka@gmail.com> Co-authored-by: pierre-1 <pierreantoine.tiberi@gmail.com>”

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
+  describe "Database table" do
+    it { is_expected.to have_db_column :confirmed }
+  end
+  
   it { is_expected.to have_many :task_items }
 end
+
