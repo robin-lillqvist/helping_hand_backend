7497c170ef1ba50f77aa9ad325a761ef4d5f5cc7
Janko <mailodjanka@gmail.com>
2020-04-16 16:21:05 +0200

    [TESTING] Refactores the index action tests for new functionality Co-authored-by: JaimeCrz <jaime.cruz.hostalot@gmail.com> Co-authored-by: Carltesio <carlos.delgado@hotmail.it>

--- spec/requests/api/v1/user_can_see_list_of_tasks_spec.rb
+++ spec/requests/api/v1/user_can_see_list_of_tasks_spec.rb
@@ -4,31 +4,21 @@ RSpec.describe "Api::V1::TasksController", type: :request do
   let(:user) { create(:user) }
   let(:user_credentials) { user.create_new_auth_token }
   let(:user_headers) { { HTTP_ACCEPT: "application/json" }.merge!(user_credentials) }
-  let!(:product) { create(:product) }
 
-  before do
-    post "/api/v1/tasks",
-         params: {
-           product_id: product.id,
-           user_id: user.id,
-         },
-         headers: user_headers
-    @task_id = (response_json)["task"]["id"]
-    @task = Task.find(@task_id)
-    put "/api/v1/tasks/#{@task.id}", params: { activity: "confirmed" }
-    post "/api/v1/tasks",
-         params: {
-           product_id: product.id,
-           user_id: user.id,
-         },
-         headers: user_headers
-    @task_id = (response_json)["task"]["id"]
-    @task_1 = Task.find(@task_id)
-    put "/api/v1/tasks/#{@task_1.id}", params: { activity: "confirmed" }
-  end
+  let(:task) { create(:task, user: user) }
+  let!(:task_items) { 5.times { create(:task_item, task: task) } }
+
+  let(:other_task) { create(:task, user: user) }
+  let!(:other_task_items) { 5.times { create(:task_item, task: other_task) } }
 
   describe "GET /tasks" do
     before do
+      put "/api/v1/tasks/#{task.id}",
+        params: { activity: "confirmed" },
+        headers: user_headers
+      put "/api/v1/tasks/#{other_task.id}",
+          params: { activity: "confirmed" },
+          headers: user_headers
       get "/api/v1/tasks"
     end
 
@@ -37,11 +27,12 @@ RSpec.describe "Api::V1::TasksController", type: :request do
     end
 
     it "returns correct number of tasks" do
+      binding.pry
       expect(response_json.count).to eq 2
     end
 
-    it "task contains a product" do
-      expect(response_json[1]["products"][0]["amount"]).to eq 1
+    it "task contains five products" do
+      expect(response_json[0]["products"].count).to eq 5
     end
   end
 end
