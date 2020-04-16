f18e261f3a485669ac955637a36338a8c45a1450
oliverochman <oliver.ochman@hotmail.com>
2020-04-16 11:12:36 +0200

    refactor req spec

--- spec/requests/api/v1/user_can_confirm_task_spec.rb
+++ spec/requests/api/v1/user_can_confirm_task_spec.rb
@@ -4,26 +4,19 @@ RSpec.describe "Api::V1::TasksController", type: :request do
   let!(:product_1) { create(:product, name: "pasta", price: 20) }
   let!(:product_2) { create(:product, name: "eggs", price: 30) }
   let!(:product_3) { create(:product, name: "tomatoes", price: 10) }
+
   let(:user) { create(:user) }
   let(:user_credentials) { user.create_new_auth_token }
   let(:user_headers) { { HTTP_ACCEPT: "application/json" }.merge!(user_credentials) }
-
-  before do
-    post "/api/v1/tasks",
-      params: {
-          product_id: product_1.id,
-          user_id: user.id
-      },
-      headers: user_headers
-    task_id = (response_json)["task"]["id"]
-    @task = Task.find(task_id)
-  end
+  
+  let(:task) { create(:task, user: user)}
+  let(:task_item) { create(:task_item, task: task, product: product_1)}
 
   describe "PUT api/v1/tasks/:id" do
     before do
-      put "/api/v1/tasks/#{@task.id}", params: { product_id: product_2.id }
-      put "/api/v1/tasks/#{@task.id}", params: { product_id: product_3.id }
-      put "/api/v1/tasks/#{@task.id}", params: { activity: "confirmed" }
+      put "/api/v1/tasks/#{task.id}", params: { product_id: product_2.id }
+      put "/api/v1/tasks/#{task.id}", params: { product_id: product_3.id }
+      put "/api/v1/tasks/#{task.id}", params: { activity: "confirmed" }
     end
 
     it "response with success message" do
@@ -31,7 +24,8 @@ RSpec.describe "Api::V1::TasksController", type: :request do
     end
 
     it "sets task attribute confirmed to true" do
-      expect(@task.reload.confirmed).to eq true
+      task.reload
+      expect(task.confirmed).to eq true
     end
   end
 end
