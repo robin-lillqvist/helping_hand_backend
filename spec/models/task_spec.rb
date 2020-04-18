1332640a4cf2dad4009666395f9730cf0361a5b7
carlos <carlos.delgado@hotmail.it>
2020-04-18 10:05:03 +0200

    adding column to schema table Co-authored-by: JaimeCrz jaime.cruz.hostalot@gmail.com Co-authored-by:  MadFarmer101 mailodjanka@gmail.com

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Task, type: :model do
   
   describe "Database table" do
     it { is_expected.to have_db_column :status }
+    it { is_expected.to have_db_column :provider_id }
   end
   
   it { is_expected.to have_many :task_items }
