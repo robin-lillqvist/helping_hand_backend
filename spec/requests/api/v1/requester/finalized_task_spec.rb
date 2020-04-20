e66b38aa330662fdd93ce5daa649bf4b53b1f5ba
Janko <mailodjanka@gmail.com>
2020-04-20 09:45:14 +0200

    Adds a spec file for requester to mark a task as finalized
    Co-authored-by: JaimeCrz <jaime.cruz.hostalot@gmail.com>
    Co-authored-by: Carltesio <carlos.delgado@hotmail.it>

---
+++ spec/requests/api/v1/requester/finalized_task_spec.rb
@@ -0,0 +1,24 @@
+RSpec.describe "PUT api/v1/tasks/:id", type: :request do
+  let(:user) { create(:user) }
+  let(:user_credentials) { user.create_new_auth_token }
+  let(:user_headers) { { HTTP_ACCEPT: "application/json" }.merge!(user_credentials) }
+
+  let(:task) { create(:task, status: "delivered") }
+  let!(:task_items) { 5.times { create(:task_item, task: task) } }
+
+  describe "Succesfully finalizes the task" do
+    before do
+      put "/api/v1/tasks/#{task.id}",
+        params: { activity: "delivered" },
+        headers: user_headers
+    end
+
+    it "returns a 200 response status" do
+      expect(response).to have_http_status 200
+    end
+
+    it "response with success message" do
+      expect(response_json["message"]).to eq "You finalized the delivery"
+    end
+  end
+end
