c2583bb56ccea1a3dfc78be390bf381459200670
carlos <carlos.delgado@hotmail.it>
2020-04-20 10:16:36 +0200

    adding the condition for an order to be finalizable in the model and task controller
    Co-authored-by: JaimeCrz <jaime.cruz.hostalot@gmail.com>
    
    Co-authored-by: MadFarmer101 <mailodjanka@gmail.com>

--- spec/requests/api/v1/requester/finalized_task_spec.rb
+++ spec/requests/api/v1/requester/finalized_task_spec.rb
@@ -1,9 +1,10 @@
 RSpec.describe "PUT api/v1/tasks/:id", type: :request do
   let(:user) { create(:user) }
+  let(:provider) { create(:user) }
   let(:user_credentials) { user.create_new_auth_token }
   let(:user_headers) { { HTTP_ACCEPT: "application/json" }.merge!(user_credentials) }
 
-  let(:task) { create(:task, status: "delivered") }
+  let(:task) { create(:task, status: "delivered", provider: provider, user: user) }
   let!(:task_items) { 5.times { create(:task_item, task: task) } }
 
   describe "Succesfully finalizes the task" do
@@ -14,11 +15,11 @@ RSpec.describe "PUT api/v1/tasks/:id", type: :request do
     end
 
     it "returns a 200 response status" do
+      binding.pry
       expect(response).to have_http_status 200
     end
 
     it "response with success message" do
-      binding.pry
       expect(response_json["message"]).to eq "You finalized the delivery"
     end
   end
