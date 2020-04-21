f056d20f1ac77373024f9ced4e8e337b0ba233d0
Jaime <jaime.cruz.hostalot@gmail.com>
2020-04-21 11:39:18 +0200

    [Migration] Added quantity to products. [Test] Updated test to check that products contains quantity.
    Co-authored-by: MadFarmer101 <mailodjanka@gmail.com>
    Co-authored-by: Carltesio <carlos.delgado@hotmail.it>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_18_150325) do
+ActiveRecord::Schema.define(version: 2020_04_21_092625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_04_18_150325) do
     t.money "price", scale: 2
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "quantity"
   end
 
   create_table "task_items", force: :cascade do |t|
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,31 +1,38 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  it "should have valid factory" do
+  it 'should have valid factory' do
     expect(create(:product)).to be_valid
   end
 
-  describe "Database table" do
+  describe 'Database table' do
     it { is_expected.to have_db_column :name }
     it { is_expected.to have_db_column :price }
+    it { is_expected.to have_db_column :quantity }
   end
 
-  describe "Validations" do
+  describe 'Validations' do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :price }
   end
 
-  describe "check if new product is being created" do
+  describe 'check if new product is being created' do
     before do
-      @product1 = Product.new(name: "bread", price: 10)
+      @product1 = Product.new(name: 'bread', price: 10, quantity: '1 piece')
     end
 
-    it "expects to contain the product name" do
-      expect(@product1.name).to eq("bread")
+    it 'expects to contain the product name' do
+      expect(@product1.name).to eq('bread')
     end
 
-    it "expects to contain the product price" do
+    it 'expects to contain the product price' do
       expect(@product1.price).to eq(10)
+    end
+
+    it 'expects to contain the product name' do
+      expect(@product1.quantity).to eq('1 piece')
     end
   end
 end
