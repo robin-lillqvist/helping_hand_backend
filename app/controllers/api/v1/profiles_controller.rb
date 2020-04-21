ae71eb8ab1062c5169f024aeb63d1440f3c6c063
Janko <mailodjanka@gmail.com>
2020-04-21 10:09:48 +0200

    Adds where statement to index action, so that user and provider can view ongoing tasks with claimed and delivered statuses
    Co-authored-by: Carltesio <carlos.delgado@hotmail.it>
    Co-authored-by: JaimeCrz <jaime.cruz.hostalot@gmail.com>

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -4,15 +4,7 @@ class Api::V1::ProfilesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    claimed_tasks = Task.where(status: %w[claimed delivered])
-    claimed_tasks.each do |task|
-      
-      if task.provider_id == current_user.id || task.user_id == current_user.id
-        return render json: claimed_tasks
-      else
-
-        return render json: { error_message: 'You are not authorized, please contact support.' }, status: 401
-      end
-    end
+    claimed_tasks = Task.where(status: %w[claimed delivered]).where(provider_id: current_user.id).or(Task.where(user_id: current_user.id))
+    render json: claimed_tasks
   end
 end
--- spec/requests/api/v1/user_can_view_task_in_profile_spec.rb
+++ spec/requests/api/v1/user_can_view_task_in_profile_spec.rb
@@ -2,6 +2,7 @@
 
 RSpec.describe "Api::V1::ProfilesController", type: :request do
   let(:user) { create(:user) }
+
   let(:requester) { create(:user) }
   let(:requester_credentials) { requester.create_new_auth_token }
   let(:requester_headers) { { HTTP_ACCEPT: "application/json" }.merge!(requester_credentials) }
@@ -10,21 +11,48 @@ RSpec.describe "Api::V1::ProfilesController", type: :request do
   let(:provider_credentials) { provider.create_new_auth_token }
   let(:provider_headers) { { HTTP_ACCEPT: "application/json" }.merge!(provider_credentials) }
 
-  let(:task) { create(:task, status: "claimed", provider: provider, user: requester) }
+  let(:another_provider) { create(:user) }
+  let(:another_provider_credentials) { another_provider.create_new_auth_token }
+  let(:another_provider_headers) { { HTTP_ACCEPT: "application/json" }.merge!(another_provider_credentials) }
+
+  let!(:task) { create(:task, status: "claimed", provider: provider, user: requester) }
   let!(:task_items) { 5.times { create(:task_item, task: task) } }
 
-  let(:another_task) { create(:task, status: "claimed", provider: provider, user: user) }
+  let!(:another_task) { create(:task, status: "delivered", provider: provider, user: user) }
   let!(:another_task_items) { 5.times { create(:task_item, task: another_task) } }
 
-  let(:another_task_2) { create(:task, status: "delivered", provider: provider, user: user) }
+  let!(:another_task_2) { create(:task, status: "confirmed", provider: provider, user: user) }
   let!(:another_task_items_2) { 5.times { create(:task_item, task: another_task_2) } }
 
-  describe "Successfully" do
+  let(:another_task_3) { create(:task, status: "claimed", provider: another_provider) }
+  let!(:task_items) { 5.times { create(:task_item, task: another_task_3) } }
 
-    describe "requester sees ongoing help request" do
+  describe "Successfully" do
+    describe "Provider sees help requests" do
       before do
         get "/api/v1/profiles",
-            headers: requester_headers
+          headers: provider_headers
+      end
+
+      it "returns a 200 response status" do
+        expect(response).to have_http_status 200
+      end
+
+      it "returns a list of claimed tasks" do
+        expect(response_json.count).to eq 2
+      end
+
+      it "returns the correct provider id" do
+        expect(response_json.first["provider_id"]).to eq provider.id
+        expect(response_json.second["provider_id"]).to eq provider.id
+        expect(response_json.last["provider_id"]).to eq provider.id
+      end
+    end
+
+    describe "Another provider views his request" do
+      before do
+        get "/api/v1/profiles",
+          headers: another_provider_headers
       end
 
       it "returns a 200 response status" do
@@ -36,10 +64,10 @@ RSpec.describe "Api::V1::ProfilesController", type: :request do
       end
     end
 
-    describe "Provider sees help request" do
+    describe "Requester views his ongoing request" do
       before do
         get "/api/v1/profiles",
-            headers: provider_headers
+          headers: requester_headers
       end
 
       it "returns a 200 response status" do
@@ -47,34 +75,7 @@ RSpec.describe "Api::V1::ProfilesController", type: :request do
       end
 
       it "returns a list of claimed tasks" do
-        expect(response_json.count).to eq 3
-      end
-
-      it "returns the correct provider id" do
-        expect(response_json.first["provider_id"]).to eq provider.id
-        expect(response_json.second["provider_id"]).to eq provider.id
-        expect(response_json.last["provider_id"]).to eq provider.id
-      end
-    end
-
-   
-
-    describe " GET /profiles unsuccessfully" do
-      let(:another_provider) { create(:user) }
-      let(:another_provider_credentials) { another_provider.create_new_auth_token }
-      let(:another_provider_headers) { { HTTP_ACCEPT: "application/json" }.merge!(another_provider_credentials) }
-
-      before do
-        get "/api/v1/profiles",
-            headers: another_provider_headers
-      end
-
-      it "should return a 401 response" do
-        expect(response).to have_http_status 401
-      end
-
-      it "should return task id" do
-        expect(response_json["error_message"]).to eq "You are not authorized, please contact support."
+        expect(response_json.count).to eq 1
       end
     end
   end
